83848f848eddd2e8174e6c8b8e39d0027644c3cb
ADNghia <ADNghia@github.com>
2017-08-03 00:10:08 +0200

    rename HomeBundle

--- GustoCoffee/src/GC/HomeBundle/Controller/HomeController.php
+++ GustoCoffee/src/GC/HomeBundle/Controller/HomeController.php
@@ -5,7 +5,7 @@ namespace GC\HomeBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DefaultController extends Controller
+class HomeController extends Controller
 {
     public function indexAction()
     {
--- GustoCoffee/vendor/sensio/generator-bundle/Generator/BundleGenerator.php
+++ GustoCoffee/vendor/sensio/generator-bundle/Generator/BundleGenerator.php
@@ -58,7 +58,7 @@ class BundleGenerator extends Generator
             $this->renderFile('bundle/Extension.php.twig', $dir.'/DependencyInjection/'.$bundle->getBasename().'Extension.php', $parameters);
             $this->renderFile('bundle/Configuration.php.twig', $dir.'/DependencyInjection/Configuration.php', $parameters);
         }
-        $this->renderFile('bundle/DefaultController.php.twig', $dir.'/Controller/DefaultController.php', $parameters);
+        $this->renderFile('bundle/HomeController.php.twig', $dir.'/Controller/HomeController.php', $parameters);
         $this->renderFile('bundle/DefaultControllerTest.php.twig', $bundle->getTestsDirectory().'/Controller/DefaultControllerTest.php', $parameters);
         $this->renderFile('bundle/index.html.twig.twig', $dir.'/Resources/views/Default/index.html.twig', $parameters);
 
