820a3a3ecfcff9f4d9f2650bf578c2cf2a554dfa
Karim BOUBRIT <karim.boubrit@neuros.fr>
2017-08-30 19:00:47 +0200

    Installing Sass and Less pre compiler, lots of work still to do, Initiating Internalazation

--- GustoCoffee/app/AppKernel.php
+++ GustoCoffee/app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
             new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+            //new JMS\TranslationBundle\JMSTranslationBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
