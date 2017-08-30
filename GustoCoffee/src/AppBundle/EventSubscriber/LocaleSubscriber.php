b59c1d4974d14c7589e3b2fa6e97dd47ec7ff37a
Karim BOUBRIT <boubrit.karim@gmail>
2017-08-30 23:48:07 +0200

    Adding language switcher, trying resolving big chunks of text to translate but failed ?

---
+++ GustoCoffee/src/AppBundle/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Karim BOUBRIT
+ * Date: 30/08/2017
+ * Time: 22:03
+ */
+
+//TODO: Clean and modify for our entity Personne
+namespace AppBundle\EventSubscriber;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    private $defaultLocale;
+
+    public function __construct($defaultLocale = 'en')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        // try to see if the locale has been set as a _locale routing parameter
+        if ($locale = $request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+            // if no explicit locale has been set on this request, use one from the session
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            // must be registered after the default Locale listener
+            KernelEvents::REQUEST => array(array('onKernelRequest', 15)),
+        );
+    }
+
+    public function localeChangerAction(Request $request, $locale)
+    {
+        return $this->redirect($request->headers->get('referer'));
+    }
+}
