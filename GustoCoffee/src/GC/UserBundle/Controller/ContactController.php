1f77f9ebb35a26af8b287f4a345e7a470e06eae9
ADNghia <ADNghia@github.com>
2017-09-07 15:22:35 +0200

    début merge

---
+++ GustoCoffee/src/GC/UserBundle/Controller/ContactController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace GC\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContactController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+        $form = $this->createForm('AppBundle\Form\ContactType', null, array(
+            'action' => $this->generateUrl('gc_contact_page'),
+            'method' => 'POST'
+        ));
+
+        if ($request->isMethod('POST')){
+            $form->handleRequest($request);
+
+            if($form->isValid()){
+                if($this->sendEmail($form->getData())){
+                    return $this->redirectToRoute('gc_contact_page');
+                } else {
+                    var_dump("error");
+                }
+            }
+        }
+
+        return $this->render('GCUserBundle:Default:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    private function sendEmail($data){
+        $myappContactMail = 'adnghia@gmail.com';
+        $myappContactPassword = 'Alexis31';
+
+
+        $transport = \Swift_SmtpTransport::newInstance('smtp.gmail.com', 465,'ssl')->setUsername($myappContactMail)->setPassword($myappContactPassword);
+
+        $mailer = \Swift_Mailer::newInstance($transport);
+        $message = \Swift_Message::newInstance($data["subject"])
+            ->setFrom(array($myappContactMail => "Message by ".$data["name"]))
+            ->setTo(array(
+                $myappContactMail => $myappContactMail
+            ))
+            ->setBody($data["message"]."\n\nEnvoyé par :".$data["email"]);
+
+        return $mailer->send($message);
+
+    }
+}
