c37a0eeab6ffd9d925fa0c82f6477cfa2a4a6447
ADNghia <ADNghia@github.com>
2017-09-15 06:51:18 +0200

    recuperation timestamp pour la réservation

--- GustoCoffee/src/GC/ReservationBundle/Controller/DefaultController.php
+++ GustoCoffee/src/GC/ReservationBundle/Controller/DefaultController.php
@@ -7,9 +7,14 @@ use AppBundle\Entity\Reservation;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class DefaultController extends Controller
 {
+    protected $dateReservation;
+    protected $heureDebut;
+    protected $heureFin;
+
     /**
      * @Route("/")
      */
@@ -31,12 +36,49 @@ class DefaultController extends Controller
 
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
+
+            $dateReservation = $reservation->getDatereservation();
+            print_r($dateReservation);
+            echo $dateReservation->date;
+            echo " ok ";
+            echo $dateReservation->getTimestamp();
+
+            $heureDebut = $reservation->getHeuredebut();
+            print_r($heureDebut);
+            echo $heureDebut->date;
+            echo " ok ";
+            echo $heureDebut->getTimestamp();
+
+            $heureFin = $reservation->getHeurefin();
+            print_r($heureFin);
+            echo $heureFin->date;
+            echo " ok ";
+            echo $heureFin->getTimestamp();
+
             $em->persist($reservation);
             $em->flush();
         }
+
+/*        $this->dateReservation = $form->get('datereservation')->getData();
+        $this->heureDebut = $form->get('datereservation')->getData();
+        $this->heureFin = $form->get('datereservation')->getData();
+
+        $this->dateReservation = date('U');
+        echo "toto ";
+        echo $this->dateReservation;
+
+        $this->heureDebut = date('U');
+        echo " toto ";
+        echo $this->heureDebut;
+
+
+        $this->heureFin = date('U');
+        echo " toto ";
+        echo $this->heureFin;*/
 
         return $this->render('GCReservationBundle:Default:reservation-private-a.html.twig', array(
             'form' => $form->createView(),
         ));
     }
+
 }
