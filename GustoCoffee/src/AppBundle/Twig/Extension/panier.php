b520e30c9d0b75be33d0cbb5c0f3b3e22ed4183d
Karim BOUBRIT <boubrit.karim@gmail>
2017-09-22 23:53:14 +0200

    Correctign slider again

---
+++ GustoCoffee/src/AppBundle/Twig/Extension/panier.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Twig\Extension;
+
+class panier extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'panier';
+    }
+}
