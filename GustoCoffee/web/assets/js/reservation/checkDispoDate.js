a15479452a613b2d97d1dc64b70970160710046a
Karim BOUBRIT <boubrit.karim@gmail>
2017-09-22 16:56:42 +0200

    Perfectionning slider for créneaux

--- GustoCoffee/web/assets/js/reservation/checkDispoDate.js
+++ GustoCoffee/web/assets/js/reservation/checkDispoDate.js
@@ -22,8 +22,6 @@ $(function() {
         //setDate: new Date()
 
     });
-
-
     // Gestion de la region fr pose problème
    // $("#datepicker").datepicker("options", "defaultDate", new Date());
 
@@ -31,16 +29,21 @@ $(function() {
 
     var min = 9; // Heure min d'ouverture du magasin
     var max = 21; // Heure max d'ouverture du magasin
+    var datePickerDate = $("#datepicker-altFormat").val();
+    var today = new Date();
+    var todayDate = $("#datepicker-altFormat").val();
+    //console.log(todayDate + ' et date picker' + datePickerDate);
 
     if (!!$('#slider-range .heureActuelleDefaut').val()) {
         var arrTime = $('#slider-range .heureActuelleDefaut').val().split(':');
         var heureActuelle = parseInt(arrTime[0], 10);
-        var minuteActuelle = arrTime[1];
-
-        if (arrTime[1] < 30) {
-            minuteActuelle = '00';
+        var minuteActuelle = parseInt(arrTime[1],10);
+        var todayDate = arrTime[2];
+        console.log(todayDate+ 'date du jours');
+        if (minuteActuelle < 30) {
+            minuteActuelle = 0;
         } else {
-            minuteActuelle = '30';
+            minuteActuelle = 30;
         }
     }else{
         console.log('pas par defaut');
@@ -59,11 +62,16 @@ $(function() {
             if ( (ui.values[0] + 55) >= ui.values[1] ) {
                 return false;
             }
-            var totalStartTime = heureActuelle*60 + parseInt(minuteActuelle,10);
-            if(ui.values[0] < totalStartTime){
-                return false;
-                console.log(ui.values[0] + ' ezesfsd ' + totalStartTime );
-                //$ ('#slider-range').children(".ui-slider-handle").first().draggable( false);
+            // Dans le cas où c'est la date du jour !
+            if ($("#datepicker-altFormat").val() == '2017-09-22' || !$("#datepicker-altFormat").val()) {
+                var totalStartTime = heureActuelle * 60 + minuteActuelle;
+                console.log(ui.values[0] + ' '+ totalStartTime);
+
+                if (ui.values[0] < totalStartTime) {
+                    return false;
+                    console.log(ui.values[0] + ' ezesfsd ' + totalStartTime);
+                    //$ ('#slider-range').children(".ui-slider-handle").first().draggable( false);
+                }
             }
 
             var hours1 = Math.floor(ui.values[0] / 60);
@@ -97,7 +105,27 @@ $(function() {
     $("#slider-range").children(".ui-slider-handle").last().text(max+':00');
 
 
+
     if($('#slider-range .heureActuelleDefaut').length && $('#slider-range .heureActuelleDefaut').val() ) {
+        setHandles(heureActuelle, minuteActuelle, min, max);
+    }
+    // Arithmétique: on calcule le nombre d'heure total et on crée les intervalles souhaité, on mettra des points ç
+    var total = (max - min ) * 2; // car 60 minutes = 2 * 30 minutes :)
+    var percent = 100 / total;
+    for (var x = 1; x < total; x++){
+        $(".ui-slider" ).append("<span class='dots' style='left:"+ x * percent + "%'></span>");
+
+    }
+
+    // Lorsqu'on change le datepicker
+    $('#datepicker').datepicker().on("change", function(e){
+        if ($("#datepicker-altFormat").val() == '2017-09-22' || !$("#datepicker-altFormat").val()){
+            setHandles(heureActuelle, minuteActuelle, min, max);
+        }
+    });
+
+    // Reinit les handles
+    function setHandles(heureActuelle, minuteActuelle, min, max){
 
         $("#slider-range").children(".ui-slider-handle").first().text(heureActuelle+':'+ minuteActuelle);
         $("#slider-range").children(".ui-slider-handle").last().text((heureActuelle+1)+':'+ minuteActuelle);
@@ -117,19 +145,9 @@ $(function() {
             console.log('Ouvre à 9h');
         }
         else{
-
-            $("#slider-range").slider('option', 'values', [heureActuelle * 60, (heureActuelle * 60) + 60]);
+            $("#slider-range").slider('option', 'values', [(heureActuelle * 60 + minuteActuelle), (heureActuelle * 60) + 60 + minuteActuelle]);
             $("#slider-range").children(".ui-slider-handle").first().text(heureActuelle + ':' + minuteActuelle);
             $("#slider-range").children(".ui-slider-handle").last().text((heureActuelle+1)  + ':' + minuteActuelle);
         }
     }
-    // Arithmétique: on calcule le nombre d'heure total et on crée les intervalles souhaité, on mettra des points ç
-    var total = (max - min ) * 2; // car 60 minutes = 2 * 30 minutes :)
-    var percent = 100 / total;
-    for (var x = 1; x < total; x++){
-        $(".ui-slider" ).append("<span class='dots' style='left:"+ x * percent + "%'></span>");
-
-    }
-
-
 });
