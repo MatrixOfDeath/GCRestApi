6a5765900fa8f05b7dff559ed4dbc357a70e99b2
ADNghia <ADNghia@github.com>
2017-09-25 17:52:56 +0200

    /salle + /produit en finalisation

--- GustoCoffee/webpack.config.js
+++ GustoCoffee/webpack.config.js
@@ -39,9 +39,12 @@ Encore
         './web/assets/scss/dashboard.scss',
         './web/assets/scss/helper.scss',
         './web/assets/scss/main.scss',
+        './web/assets/scss/produit.scss',
+        './web/assets/scss/tunel.scss',
         './web/assets/scss/coffee-animation.scss',
         './web/assets/scss/coffee-loader.scss',
-        './web/assets/scss/reservation/checkDispoDate.scss'
+        './web/assets/scss/reservation/checkDispoDate.scss',
+
     ])
 
     // allow sass/scss files to be processed
