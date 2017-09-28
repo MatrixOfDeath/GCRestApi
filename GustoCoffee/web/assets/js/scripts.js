2017912adfc2f9065b4a0c0b5522578a59617b6c
Karim BOUBRIT <boubrit.karim@gmail.com>
2017-09-28 19:29:59 +0200

    Adding menu design, adding corrections

--- GustoCoffee/web/assets/js/scripts.js
+++ GustoCoffee/web/assets/js/scripts.js
@@ -4,3 +4,19 @@
 $(function(){
     $('a.tc-link[title]').tooltip();
 });
+
+
+$(document).ready(function(){
+    $(window).on("scroll",function(){
+        var wn = $(window).scrollTop();
+        if(wn > 80){
+            $('#mainNav').addClass('menu-blanc');
+            //$(".navbar").css({"background-color":"#f8f8f8", "border-color": "#f8f8f8"}).fadeIn();
+
+        }
+        else{
+            $('#mainNav').removeClass('menu-blanc');
+            //$(".navbar").css({"background-color":"transparent", "border-color": "transparent"}).fadeIn();
+        }
+    });
+});
