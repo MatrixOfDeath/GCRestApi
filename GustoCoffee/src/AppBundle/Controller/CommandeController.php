4a85d7675752c7448b9dce975a921731ad80680b
Karim BOUBRIT <boubrit.karim@gmail>
2017-10-06 04:29:30 +0200

    FO ... need to finishe payment, facture, corrections

--- GustoCoffee/src/AppBundle/Controller/CommandeController.php
+++ GustoCoffee/src/AppBundle/Controller/CommandeController.php
@@ -190,6 +190,8 @@ class CommandeController extends FOSRestController
         $totalTVA = 0;
         $totalSalleHT = 0;
         $totalSalleTVA = 0;
+        $totalSalleTTC = 0;
+        $thirdHourFree = 0;
 
         $facturation = $em->getRepository('AppBundle:UtilisateursAdresses')->find($adresse['facturation']);
         $livraison = $em->getRepository('AppBundle:UtilisateursAdresses')->find($adresse['livraison']);
@@ -218,29 +220,64 @@ class CommandeController extends FOSRestController
                 'prixHT' => round($produit->getPrixproduit(),2),
                 'prixTTC' => round($produit->getPrixproduit() / $produit->getTva()->getMultiplicate(),2));
         }
+
+
+
+
         /**
          * fin de s'y retrouver IDE
          * @var $salle \AppBundle\Entity\Salle
          */
-        foreach($salles as $salle)
-        {
-            $prixSalleHT = ($salle->getPrixsalle() * $panier_salle[$salle->getIdsalle()]['totalHeures']);
-            $prixSalleTTC = ($salle->getPrixsalle() * $panier_salle[$salle->getIdsalle()]['totalHeures'] / $salle->getTva()->getMultiplicate());
+        foreach($salles as $salle) {
+            // Complexity * 4 / 2 + Raisonnement -42
+            $total30Minutes = $panier_salle[$salle->getIdsalle()]['totalHeures'] * 2;
+            $totaleHeuresR = $panier_salle[$salle->getIdsalle()]['totalHeures'];
+            $totaleMinutesR = $panier_salle[$salle->getIdsalle()]['totalMinutes'];
+
+            if ($panier_salle[$salle->getIdsalle()]['totalMinutes'] >= 30) {
+                $totalMinutes = (2 * $salle->getCapacitymax());
+            } else {
+
+                $totalMinutes = 0;
+            }
+
+            if ($totaleHeuresR >= 5 or ($totaleHeuresR >= 5 and  $totaleMinutesR >= 30) ) {
+                $totalSalleTTC = $totalSalleTTC + $salle->getPrixsalle() * 4;
+                $prixSalleTTC = $salle->getPrixsalle() * 4;
+
+            }
+            else if( ($totaleHeuresR == 3 and $total30Minutes >= 30) or $totaleHeuresR >= 4 ) {
+                $totalSalleTTC =  $totalSalleTTC + $salle->getPrixsalle() + (($total30Minutes - 2 )* 2 * $salle->getCapacitymax()) + $totalMinutes - (2 * $salle->getCapacitymax());
+                $thirdHourFree += (2 * $salle->getCapacitymax());
+                $prixSalleTTC = $salle->getPrixsalle() + (($total30Minutes - 2 )* 2 * $salle->getCapacitymax()) + $totalMinutes - (2 * $salle->getCapacitymax());
+            } else{
+                $totalSalleTTC =  $totalSalleTTC + $salle->getPrixsalle() + (($total30Minutes - 2) * 2 * $salle->getCapacitymax()) + $totalMinutes;
+                $prixSalleTTC =  $salle->getPrixsalle() + (($total30Minutes - 2) * 2 * $salle->getCapacitymax()) + $totalMinutes;
+            }
+
+            $prixSalleHT = $prixSalleTTC / (2 - $salle->getTva()->getMultiplicate()) ;
+            $tvaSalle = $prixSalleTTC - $prixSalleHT;
             $totalSalleHT += $prixSalleHT;
 
-            if (!isset($commande['tva']['%'.$salle->getTva()->getValeur()]))
-                $commande['tva']['%'.$salle->getTva()->getValeur()] = round($prixSalleTTC - $prixSalleHT,2);
+            if (!isset($commande['tvaSalle']['%'.$salle->getTva()->getValeur()]))
+                $commande['tvaSalle']['%'.$salle->getTva()->getValeur()] = round($tvaSalle,2);
             else
-                $commande['tva']['%'.$salle->getTva()->getValeur()] += round($prixSalleTTC - $prixSalleHT,2);
+                $commande['tvaSalle']['%'.$salle->getTva()->getValeur()] += round($tvaSalle,2);
 
             $totalSalleTVA += round($prixSalleTTC - $prixSalleHT,2);
 
-            $commande['salle'][$salle->getIdsalle()] = array('reference' => $salle->getNomsalle(),
-                'quantite' => $panier_salle[$salle->getIdsalle()]['totalHeures'],
+            $commande['salle'][$salle->getIdsalle()] = array(
+                'reference' => $salle->getNomsalle(),
+                'heures' => $totaleHeuresR,
+                'minutes' => $totaleMinutesR,
+                'idReservation' => $panier_salle[$salle->getIdsalle()]['idReservation'],
                 'prixHT' => round($salle->getPrixsalle(),2),
-                'prixTTC' => round($salle->getPrixsalle() / $salle->getTva()->getMultiplicate(),2));
+                'prixTTC' => round($prixSalleTTC,2),
+
+            );
         }
 
+        // Pourquoi j'ai ajouté livraison ? useless ><
         $commande['livraison'] = array('prenom' => $livraison->getPrenom(),
             'nom' => $livraison->getNom(),
             'telephone' => $livraison->getTelephone(),
@@ -250,7 +287,6 @@ class CommandeController extends FOSRestController
             'pays' => $livraison->getPays(),
             'complement' => $livraison->getComplement()
         );
-
 
         $commande['facturation'] = array('prenom' => $facturation->getPrenom(),
             'nom' => $facturation->getNom(),
@@ -265,8 +301,10 @@ class CommandeController extends FOSRestController
         $commande['prixHT'] = round($totalHT,2);
         $commande['prixTTC'] = round($totalHT + $totalTVA,2);
         $commande['prixSalleHT'] = round($totalSalleHT,2);
-        $commande['prixSalleTTC'] = round($totalSalleHT + $totalSalleHT,2);
+        $commande['prixSalleTTC'] = round($totalSalleTTC,2);
+        $commande['totalTTC'] =  $commande['prixTTC'] + $commande['prixSalleTTC'];
         $commande['token'] = bin2hex(random_bytes(20));
+        $commande['thirdHourFree'] = round($thirdHourFree, 2);
 
         return $commande;
     }
@@ -290,10 +328,11 @@ class CommandeController extends FOSRestController
         $commande->setValider(0);
         $commande->setReference(0);
         $commande->setCommande($this->facture($session));
+       //todo récupéré l'id reservation ! $commande->setReservation();
 
         if (!$session->has('commande')) {
             $em->persist($commande);
-            $session->set('commande',$commande);
+            $session->set('commande', $commande);
         }
 
         $em->flush();
@@ -353,37 +392,55 @@ class CommandeController extends FOSRestController
      */
     public function validationPaymentAction(Request $request, SessionInterface $session, $id){
 
-        \Payplug\Payplug::setSecretKey('sk_test_44dfjjzLQ6f0S4xqYxGlQR');
+        if ($request->getMethod() == 'POST' && $session->has('commande'))
+        {
+            $commande = $session->get('commande');
+           if ($request->get('token') == $commande['token']){
+               $totalPrix = $request->request->get('totalTTC');
 
-        $amount = 200;
-        $email = "gustocoffee+official@gmail.com";
-        $commande_id = $id;
-        /**
-         * Vérifier si on est connecté !!
-         * @var $customer_id \AppBundle\Entity\Personne
-         */
-        $customer_id = $this->getUser();
-        $payment = \Payplug\Payment::create(array(
-            'amount'           => $amount * 100,
-            'currency'         => 'EUR',
-            'customer'         => array(
-                'email'          => $email
-            ),
-            'hosted_payment'   => array(
-                'return_url'     => 'http://dev.gc.fr/app_dev.php/fr/commande/api/banque/'.$commande_id,
-                'cancel_url'     => 'http://dev.gc.fr/app_dev.php/fr/commande/paiement_commande/'.$commande_id
-            ),
-            'notification_url' => 'http://dev.gc.fr/app_dev.php/fr/commande/notifications?id='.$commande_id,
-            'metadata'         => array(
-                'customer_id'    => $this->getUser()->getId(),
+               \Payplug\Payplug::setSecretKey('sk_test_44dfjjzLQ6f0S4xqYxGlQR');
 
-            )
-        ));
+               $amount = $totalPrix * 100; // En centime selon la doc de Payplug
+               $email = "gustocoffee+official@gmail.com";
+               $commande_id = $id;
+               /**
+                * Vérifier si on est connecté !!
+                * @var $customer_id \AppBundle\Entity\Personne
+                */
+               $customer_id = $this->getUser();
+               $payment = \Payplug\Payment::create(array(
+                   'amount'           => $amount * 100,
+                   'currency'         => 'EUR',
+                   'customer'         => array(
+                       'email'          => $email
+                   ),
+                   'hosted_payment'   => array(
+                       'return_url'     => 'http://dev.gc.fr/app_dev.php/fr/commande/api/banque/'.$commande_id,
+                       'cancel_url'     => 'http://dev.gc.fr/app_dev.php/fr/commande/paiement_commande/'.$commande_id
+                   ),
+                   'notification_url' => 'http://dev.gc.fr/app_dev.php/fr/commande/notifications?id='.$commande_id,
+                   'metadata'         => array(
+                       'customer_id'    => $this->getUser()->getId(),
 
-        $payment_url = $payment->hosted_payment->payment_url;
-        $payment_id = $payment->id;
+                   )
+               ));
 
-        return $this->redirect($payment_url);
+               $payment_url = $payment->hosted_payment->payment_url;
 
+               //Todo: envoyer l'id payment ! somewhere
+               $payment_id = $payment->id;
+
+               return $this->redirect($payment_url);
+
+           }else{
+               // Token envoyé par l'user invalide !!
+               return $this->redirect($this->generateUrl('validation_panier'));
+
+           }
+
+        }else{
+            return $this->redirect($this->generateUrl('validation_panier'));
+        }
     }
+
 }
