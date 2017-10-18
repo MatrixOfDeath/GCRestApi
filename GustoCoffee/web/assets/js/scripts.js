052d0f8656dca7a0cd11c31ee94738f51c41dd6b
Karim BOUBRIT <boubrit.karim@gmail>
2017-10-18 15:04:06 +0200

    Adding new menu

--- GustoCoffee/web/assets/js/scripts.js
+++ GustoCoffee/web/assets/js/scripts.js
@@ -2,11 +2,7 @@
 
 
 $(function(){
-    $('ul.nav li.dropdown').hover(function() {
-        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-    }, function() {
-        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
-    });
+
 
     $('a.tc-link[title]').tooltip();
 });
@@ -15,16 +11,31 @@ $(function(){
 $(document).ready(function(){
 
     // $("#main").onepage_scroll();
+    $('ul.nav li.dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+    }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+    });
 
     $(window).on("scroll",function(){
         var wn = $(window).scrollTop();
+        // if(wn > 30){
+        //     $('#mainNav').css({"margin-top" : 0}).fadeIn("slow");
+        // }else{
+        //     $('#mainNav').css({"margin-top" : 20}).fadeIn("slow");
+        // }
         if(wn > 50){
             $('#mainNav').addClass('menu-blanc');
-            //$(".navbar").css({"background-color":"#f8f8f8", "border-color": "#f8f8f8"}).fadeIn();
+            $('#mainNav').removeClass('marginTop');
+            $('#mainNav').css({"width" : "100%"}).fadeIn("slow");
 
+            //$(".navbar").css({"background-color":"#f8f8f8", "border-color": "#f8f8f8"}).fadeIn();
         }
         else{
             $('#mainNav').removeClass('menu-blanc');
+            $('#mainNav').addClass('marginTop');
+            $('#mainNav').css({"width" : "98%"}).fadeIn("slow");
+
             //$(".navbar").css({"background-color":"transparent", "border-color": "transparent"}).fadeIn();
         }
     });
