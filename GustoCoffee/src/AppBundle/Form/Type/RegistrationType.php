16180a3a6f76905bb57a51fbe2f831fa5f2a9843
Karim BOUBRIT <boubrit.karim@gmail>
2017-10-30 18:43:57 +0100

    Favicos and minor debug

--- GustoCoffee/src/AppBundle/Form/Type/RegistrationType.php
+++ GustoCoffee/src/AppBundle/Form/Type/RegistrationType.php
@@ -5,11 +5,7 @@ namespace AppBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RegistrationType extends AbstractType
